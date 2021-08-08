902b21efe81a8c113e4233a52c352d309a76ce4a
Hidenaga41 <hidenaga.u@gmail.com>
2021-08-08 10:32:22 +0900

    add roslibjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useROS } from "./ROS";
+import "./App.css";
 
 function App() {
+  const {
+    isConnected,
+    topics,
+    url,
+    changeUrl,
+    toggleConnection,
+    toggleAutoconnect,
+  } = useROS();
+
+  console.log(topics);
+
+  const defaultURL = "ws://0.0.0.0:9090";
+
+  useEffect(() => {
+    console.log("Toggle Connect is first ");
+    if (url !== defaultURL) {
+      changeUrl(defaultURL);
+    }
+
+    if (!isConnected) {
+      toggleAutoconnect();
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        <b>Simple connect: </b>
+        <button onClick={toggleConnection}>Toggle Connect</button> <br />
+        <b>ROS url input: </b>
+        <input
+          name="urlInput"
+          defaultValue={url}
+          onChange={(event) => changeUrl(event.target.value)}
+        />
+        <br />
+        <b>ROS url to connect to: </b> {url} <br />
+        <b>Status of ROS:</b> {isConnected ? "connected" : "not connected"}
+        <br />
+        <b>Topics detected:</b>
+        <br />
+      </p>
     </div>
   );
 }
